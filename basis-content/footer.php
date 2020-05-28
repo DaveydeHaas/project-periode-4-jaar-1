e6f6e50cb06f292a364e61a3360958159332b0a8
Phew <phew2002@gmail.com>
2020-05-28 10:47:39 +0200

    28/05 #1

---
+++ basis-content/footer.php
@@ -0,0 +1,97 @@
+<!-- Footer -->
+<footer class="page-footer footer-color2">
+
+
+    <div class="container-fluid footer-color1 ">
+
+        <!-- Grid row-->
+        <div class="row py-4 d-flex align-items-center">
+        </div>
+        <!-- Grid row-->
+
+    </div>
+
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+
+        <!-- Grid row -->
+        <div class="row mt-3">
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 footer-color3">
+
+                <!-- Content -->
+                <h6 class="text-uppercase font-weight-bold">Game District</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Koop al uw game merchandise voordelig online! Dé online specialist in game kleding en game merchandise!</p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 footer-color3">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">Producten</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#!">Figuren</a>
+                </p>
+                <p>
+                    <a href="#!">Kleding</a>
+                </p>
+                <p>
+                    <a href="#!"></a>
+                </p>
+                <p>
+                    <a href="#!"></a>
+                </p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 footer-color3">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">Informatie</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#!">Verzending & bezorging</a>
+                </p>
+                <p>
+                    <a href="#!">Terugstuurbeleid</a>
+                </p>
+                <p>
+                    <a href="#!"></a>
+                </p>
+                <p>
+                    <a href="#!"></a>
+                </p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4 footer-color3">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">Contact</h6>
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Utrecht, AC 1111, NL</p>
+                <p>contact@gamedistrict.com</p>
+                <p>+ 06 234 567 88</p>
+                <p>+ 06 234 567 89</p>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
