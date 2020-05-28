aaa73777f51465a8d6bd37b7aadaa4fb5b7e1177
Phew <phew2002@gmail.com>
2020-05-28 12:26:54 +0200

    28/05 #3

--- basis-content/footer.php
+++ basis-content/footer.php
@@ -1,16 +1,5 @@
 <!-- Footer -->
-<footer class="page-footer footer-color2 footer_background">
-
-
-    <div class="container-fluid footer-color1 ">
-
-        <!-- Grid row-->
-        <div class="row py-4 d-flex align-items-center">
-        </div>
-        <!-- Grid row-->
-
-    </div>
-
+<footer class="page-footer footer-color1 footer-top">
 
     <!-- Footer Links -->
     <div class="container text-center text-md-left mt-5">
@@ -19,22 +8,22 @@
         <div class="row mt-3">
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 footer-color3">
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 footer-text">
 
                 <!-- Content -->
                 <h6 class="text-uppercase font-weight-bold">Game District</h6>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <hr class="footer-color2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
                 <p>Koop al uw game merchandise voordelig online! Dé online specialist in game kleding en game merchandise!</p>
 
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 footer-color3">
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 footer-text">
 
                 <!-- Links -->
                 <h6 class="text-uppercase font-weight-bold">Producten</h6>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <hr class="footer-color2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
                 <p>
                     <a href="#!">Figuren</a>
                 </p>
@@ -52,19 +41,19 @@
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 footer-color3">
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 footer-text">
 
                 <!-- Links -->
                 <h6 class="text-uppercase font-weight-bold">Informatie</h6>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <hr class="footer-color2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+               
+                    <li><a href="index.php?content=home">Verzending & bezorging</a></li>
+                
+               
+                    <li><a href="index.php?content=home">Terugstuurbeleid</a></li>
+               
                 <p>
-                    <a href="#!">Verzending & bezorging</a>
-                </p>
-                <p>
-                    <a href="#!">Terugstuurbeleid</a>
-                </p>
-                <p>
-                    <a href="#!"></a>
+                    <li><a href="#!">Service Voorwaarden</a></li>
                 </p>
                 <p>
                     <a href="#!"></a>
@@ -74,11 +63,11 @@
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4 footer-color3">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4 footer-text">
 
                 <!-- Links -->
                 <h6 class="text-uppercase font-weight-bold">Contact</h6>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <hr class="footer-color2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
                 <p>Utrecht, AC 1111, NL</p>
                 <p>contact@gamedistrict.com</p>
                 <p>+ 06 234 567 88</p>
--- content/registratie.php
+++ content/registratie.php
@@ -1,1 +1,61 @@
-Registratie
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3"></div>
+
+        <div class="col-6">
+            <!-- Material form register -->
+            <div class="card">
+
+                <h5 class="card-header info-color white-text text-center py-4">
+                    <strong>Registreren</strong>
+                </h5>
+
+                <!--Card content-->
+                <div class="card-body px-lg-5 pt-0">
+
+                    <!-- Form -->
+                    <form class="text-center" style="color: #757575;" action="#!">
+
+                            
+                    
+                        <!-- E-mail -->
+                        <div class="md-form mt-0">
+                            <input type="email" id="materialRegisterFormLastName" class="form-control">
+                            <label for="materialRegisterFormLastName">Gebruikersnaam</label>
+                        </div>
+
+                        <!-- E-mail -->
+                        <div class="md-form mt-0">
+                            <input type="email" id="materialRegisterFormEmail" class="form-control">
+                            <label for="materialRegisterFormEmail">E-mail</label>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="md-form">
+                            <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                            <label for="materialRegisterFormPassword">Wachtwoord</label>
+                        </div>
+
+
+                        <!-- Sign up button -->
+                        <button class="btn btn-outline-dark btn-rounded btn-block my-4" type="submit">Aanmelden</button>
+
+                    
+
+                        <!-- Terms of service -->
+                        <p>Door op
+                            <b>Aanmelden</b> te klikken, gaat u akkoord met onze
+                            <a href="" target="_blank">verservice voorwaarden</a>
+
+                    </form>
+                    <!-- Form -->
+
+                </div>
+
+            </div>
+            <!-- Material form register -->
+        </div>
+
+        <div class="col-3"></div>
+    </div>
+</div>
