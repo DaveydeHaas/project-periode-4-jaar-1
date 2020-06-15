0e5954747b84b153286534992f74f69059bd321c
DaveydeHaas <daveydehaas14@gmail.com>
2020-06-15 20:48:43 +0200

    cards gemaakt voor producten

--- content/webshop.php
+++ content/webshop.php
@@ -1,30 +1,157 @@
-<div class="col-6">
-      </div>
-      <div class="dropdown">
-            <button class="icon-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Test</button>
-            <div class="dropdown-content">
-                <div class="header">
-                    <h2>Mega Menu</h2>
-                </div>   
-                <div class="row">
-                    <div class="column">
-                    <h3>Category 1</h3>
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                    </div>
-                    <div class="column">
-                    <h3>Category 2</h3>
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                    </div>
-                    <div class="column">
-                    <h3>Category 3</h3>
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                    </div>
+<div class="container">
+    <div class="row">
+        <div class="col-12 navbar-to-content-fix"></div>
+        <!-- 1 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
                 </div>
-            </div>
+                </div>
+            </form>
+        </div>
+                <!-- 2 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 3 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 4 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 5 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 6 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 7 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+                <!-- 8 -->
+        <div class="col-md-3 col-sm6">
+            <form action="./content/webshop-add.php" method="POST" >
+                <div class="card card-webshop">
+                    <img src="./afbeeldingen/producten/LeagueOfLegends01.png" alt="chibi" class="img-fluid card-img-top image-styling">
+                <div class="card-body">
+                <h5 class="card-title">Product1</h5>
+                    <hr>
+                    <p class="card-text">
+                         Some quick example text to build on the card.
+                    </p>
+                    <h5 class="price-placement">
+                    <button type="submit" name="add" class="btn btn-warning btn-placement">Add to Cart</button>
+                    <span class="price">$400</span>
+                    </h5>
+                </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
