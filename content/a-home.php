dd2e3b0e5fb050dd19a87b106ce3259d0eda6c11
DaveydeHaas <daveydehaas14@gmail.com>
2020-07-01 14:10:40 +0200

    admin pannels beveiligd

--- content/a-home.php
+++ content/a-home.php
@@ -1,5 +1,21 @@
 a-home
 <?php
+$userrole = 'admin';
+
+if(isset($_SESSION['id'])){
+    if($_SESSION['userrole'] == $userrole){
+        echo 'Test';
+    }
+
+}else{
+    header("Location: ./index.php?content=message&alert=no-admin");
+}
+
+
+
+
+
+
     var_dump($_SESSION);
     
     echo session_id();
--- content/a-users.php
+++ content/a-users.php
@@ -1,1 +1,13 @@
-1
+<?php
+$userrole = 'admin';
+
+if(isset($_SESSION['id'])){
+    if($_SESSION['userrole'] == $userrole){
+        echo 'Test';
+    }
+
+}else{
+    header("Location: ./index.php?content=message&alert=no-admin");
+}
+
+?>
